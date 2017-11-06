694b99298bbf875f7261d9844342b34c9a51c4b0
Guest User <Guest@Epicodus-3E.local>
2017-11-06 14:18:04 -0800

    generate random letters within gameboard according to standard boggle letter distribution

--- app/src/main/java/com/epicodus/boggle/GameboardActivity.java
+++ app/src/main/java/com/epicodus/boggle/GameboardActivity.java
@@ -18,6 +18,18 @@ public class GameboardActivity extends Activity {
     @Bind(R.id.button2) Button mButton2;
     @Bind(R.id.button3) Button mButton3;
     @Bind(R.id.button4) Button mButton4;
+    @Bind(R.id.button5) Button mButton5;
+    @Bind(R.id.button6) Button mButton6;
+    @Bind(R.id.button7) Button mButton7;
+    @Bind(R.id.button8) Button mButton8;
+    @Bind(R.id.button9) Button mButton9;
+    @Bind(R.id.button10) Button mButton10;
+    @Bind(R.id.button11) Button mButton11;
+    @Bind(R.id.button12) Button mButton12;
+    @Bind(R.id.button13) Button mButton13;
+    @Bind(R.id.button14) Button mButton14;
+    @Bind(R.id.button15) Button mButton15;
+    @Bind(R.id.button16) Button mButton16;
     ArrayList<Button> allButtons = new ArrayList<Button>();
 
 
@@ -26,10 +38,8 @@ public class GameboardActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameboard);
         ButterKnife.bind(this);
-        allButtons.add(mButton1);
-        allButtons.add(mButton2);
-        allButtons.add(mButton3);
-        allButtons.add(mButton4);
+
+        createButtonList();
 
         for(Button button : allButtons) {
             String letter = randomLetter();
@@ -38,9 +48,30 @@ public class GameboardActivity extends Activity {
 
     }
 
+    public void createButtonList() {
+        allButtons.add(mButton1);
+        allButtons.add(mButton2);
+        allButtons.add(mButton3);
+        allButtons.add(mButton4);
+        allButtons.add(mButton5);
+        allButtons.add(mButton6);
+        allButtons.add(mButton7);
+        allButtons.add(mButton8);
+        allButtons.add(mButton9);
+        allButtons.add(mButton10);
+        allButtons.add(mButton11);
+        allButtons.add(mButton12);
+        allButtons.add(mButton13);
+        allButtons.add(mButton14);
+        allButtons.add(mButton15);
+        allButtons.add(mButton16);
+    }
+
+
+
     public String randomLetter() {
         Random random = new Random();
-        int randomNum = random.nextInt(150) + 0;
+        int randomNum = random.nextInt(150);
         String randomLetter = letterArray.get(randomNum);
         return randomLetter;
     }
